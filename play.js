e20d457264f0a9aef9309e611a1a48fb3800966e
Samantha Fok <samantha.fok@hotmail.com>
2020-11-04 21:14:08 +0000

    Added my name to identify my snake

--- play.js
+++ play.js
@@ -1,26 +1,3 @@
-// const net = require('net');
-
-// /**
-//  * Establishes connection with the game server
-//  */
-// const connect = function() {
-//   const conn = net.createConnection({ 
-//     host: '135.23.222.131',
-//     port: 50542
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8'); 
-  
-//   conn.on('data', (data) => {
-//     console.log('Server says: ', data);
-//   });
-
-//   return conn;
-// }
-
-
-// console.log('Connecting ...');
-// connect();
 
 // play.js
 const  connect  = require('./client');
