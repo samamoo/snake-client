8f3fa1013bd43b6cc67d8e131edc08bfc0474fdc
Samantha Fok <samantha.fok@hotmail.com>
2020-11-04 20:52:03 +0000

    Play file for snake game requireing function from client.js

--- play.js
+++ play.js
@@ -1,23 +1,28 @@
-const net = require('net');
+// const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+// /**
+//  * Establishes connection with the game server
+//  */
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: '135.23.222.131',
+//     port: 50542
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); 
   
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
+//   conn.on('data', (data) => {
+//     console.log('Server says: ', data);
+//   });
 
-  return conn;
-}
+//   return conn;
+// }
 
 
+// console.log('Connecting ...');
+// connect();
+
+// play.js
+const  connect  = require('./client');
 console.log('Connecting ...');
 connect();
