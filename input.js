f469795d3dee6ca7e1efc70825983713090a290b
Samantha Fok <samantha.fok@hotmail.com>
2020-11-05 02:33:12 +0000

    Set WASD keys to move the snake

--- input.js
+++ input.js
@@ -1,24 +1,47 @@
-// const  connect  = require('./play');
-// let connection;
+const  connect = require('./play');
+let connection;
 
 const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up")
+  } else if (data === "a") {
+    connection.write("Move: left")
+  } else if (data === "s") {
+    connection.write("Move: down")
+  } else if (data === "d") {
+    connection.write("Move: right")
   }
 }
 
-const setupInput = function() {
-  // connection = conn;
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-
   stdin.on('data', handleUserInput)
 
   return stdin;
 }
 
-setupInput()
+// setupInput(connect())
+
+// conn.on('connect', () => {
+
+//   // setTimeout(() => {
+//   //   conn.write('Move: up');
+//   // }, 1000)
+//   // setTimeout(() => {
+//   //   conn.write('Move: left');
+//   // }, 100);
+//   // setInterval(() => {
+//   //   conn.write('Move: left');
+//   // }, 50)
+//   //conn.write('Move: right');
+
+// })
 
 module.exports = setupInput;
